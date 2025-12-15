16a41076f7cfbe11b395066216dfc3cc858d1977
kj-bonami <krishna.jha@bonamisoftware.com>
2025-12-15 11:36:51 +0530

    minor cosmetic fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
         <Text<"h1"> as="h1" className={styles.title}>{"WEBKEMY"}</Text>
         <Text<"p"> as="p" className={styles.desc}>{"Minimal. Accessible. Easily-Extensible. React/NextJS Components."}</Text>
         <Button<"a"> variant="primary" href="/components">{"Components"}</Button>
-        {/* <div className={styles.blob}></div> */}
       </div>
     </main>
   );
