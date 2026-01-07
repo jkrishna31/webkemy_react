683e7022a81d1d9846656d28fd367c379b7169cd
kj-bonami <krishna.jha@bonamisoftware.com>
2026-01-08 01:05:40 +0530

    primary text color fix

--- src/constants/colors.const.ts
+++ src/constants/colors.const.ts
@@ -1,5 +1,5 @@
 export const THEME_BG_LIGHT = "rgb(252, 254, 255)";
-export const THEME_BG_DARK = "rgb(25, 27, 27)";
+export const THEME_BG_DARK = "rgb(24, 26, 27)";
 
 export const colors = {
   RED: "red",
